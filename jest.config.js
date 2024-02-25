973fb07c4526ec901cda52282d3f137a6c776901
Lee <leesjensen@gmail.com>
2024-02-24 17:51:03 -0700

    enhance(jest) require 100%

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('jest').Config} */
+const config = {
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: -10,
+    },
+  },
+};
+
+module.exports = config;
